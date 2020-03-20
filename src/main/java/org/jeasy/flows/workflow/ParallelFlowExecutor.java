d1596a602f98f4ee4713a2005a24167815621453
Daniel Bulger <54212920+danielbulger@users.noreply.github.com>
2020-03-20 11:58:16 +1100

    Changed ParallelFlowExecutor#executeInParallel to use a CountDownLatch to wait for work units completion.
    This removes the constant polling for unit completion.

--- src/main/java/org/jeasy/flows/workflow/ParallelFlowExecutor.java
+++ src/main/java/org/jeasy/flows/workflow/ParallelFlowExecutor.java
@@ -31,6 +31,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
@@ -50,27 +51,35 @@ class ParallelFlowExecutor {
     List<WorkReport> executeInParallel(List<Work> works, WorkContext workContext) {
         // submit work units to be executed in parallel
         Map<Work, Future<WorkReport>> reportFutures = new HashMap<>();
+
+        CountDownLatch latch = new CountDownLatch(works.size());
+
         for (Work work : works) {
-            Future<WorkReport> reportFuture = workExecutor.submit(() -> work.call(workContext));
+            Future<WorkReport> reportFuture = workExecutor.submit(() -> {
+                try {
+                    return work.call(workContext);
+                } finally {
+                    latch.countDown();
+                }
+            });
             reportFutures.put(work, reportFuture);
         }
 
-        // poll for work completion
-        int finishedWorks = works.size();
-        // FIXME polling futures for completion, not sure this is the best way to run callables in parallel and wait for them to complete (use CompletionService??)
-        while (finishedWorks > 0) {
-            for (Future<WorkReport> future : reportFutures.values()) {
-                if (future != null && future.isDone()) {
-                        finishedWorks--;
-                }
-            }
+        try {
+            // wait for the work completion
+            latch.await();
+        } catch(InterruptedException e) {
+            LOGGER.log(Level.WARNING, "Unable to complete all work units", e);
         }
 
         // gather reports
         List<WorkReport> workReports = new ArrayList<>();
         for (Map.Entry<Work, Future<WorkReport>> entry : reportFutures.entrySet()) {
             try {
-                workReports.add(entry.getValue().get());
+                // If the CountDownLatch was interrupted not all work may have completed.
+                if(entry.getValue().isDone()) {
+                    workReports.add(entry.getValue().get());
+                }
             } catch (InterruptedException | ExecutionException e) {
                 LOGGER.log(Level.WARNING, "Unable to get report of work unit ''{0}''", entry.getKey().getName());
             }
